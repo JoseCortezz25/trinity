6647e0cec7522a1ceeb99dbad3f8dc733d9cf37d
Gilber Cuadrado <cuadradogilber@gmail.com>
2022-09-27 07:38:22 -0500

    Se realizó vista primaria de plataforma

--- src/pages/Plataform/Plataform.jsx
+++ src/pages/Plataform/Plataform.jsx
@@ -1,12 +1,60 @@
 import React from "react";
+import YoutubeIcon from '../../assets/img/youTube.png'
+import WebIcon from '../../assets/img/Web.png'
+import ImgBack from '../../assets/img/back-end.jpg'
+import ImgFront from '../../assets/img/Front-end.jpg'
+import Imgrutes from '../../assets/img/img_rutes.jpg'
 import "./Plataform.css";
 
 const Plataform = () => {
   return (
     <main className="Plataform">
-      Plataform
+      <h2 className="tittleh2">Learning Paths</h2>
+      <div className="Content_paths">
+        <CardPath image={ImgFront} title="Front-End Rute" description="Hola dev, aquí encontrarás la ruta sugerida por Trinity FS si deseas convertirte en desarrollador Front-End <Disfruta del proceso />" />
+        <CardPath image={ImgBack} title="Back-End Rute" description="Hola dev, aquí encontrarás la ruta sugerida por Trinity FS si deseas convertirte en desarrollador Back-End <Disfruta del proceso />" />
+        <CardPath image={Imgrutes} title="Complements" description="Hola dev, Aquí encontrarás temas que te puede ayudar de complemento  <Disfruta del proceso />" />
+      </div>
+
+      <h2 className="tittleh2">Recomended Resources</h2>
+      <div className="RecomemdedCards">
+        <Recomended tittle="Udemy" subtitle="Fransisco" typeSite='sitioweb' />
+        <Recomended tittle="YouTube" subtitle="Minudev" typeSite='youtube' />
+        <Recomended tittle="YouTube" subtitle="HolaMundo" typeSite='youtube' />
+        <Recomended tittle="Platzi" subtitle="Freddy" typeSite='sitioweb' />
+        <Recomended tittle="Web" subtitle="Profe Alex" typeSite='sitioweb' />
+        <Recomended tittle="Platzi" subtitle="MDN Web Docs" typeSite='youtube' />
+      </div>
     </main>
   );
 };
+
+const CardPath = ({ title, description, image }) => (
+  <div className="contenedorTargetas">
+    <div className="contenedorImage">
+      <img src={image} alt="" className="Imgdev" />
+      <h2 className="text">{title}</h2>
+    </div>
+    <p className="textDescription">{description}</p>
+  </div>
+);
+
+const Recomended = ({ tittle, subtitle, typeSite }) => (
+  <div className="Recomended">
+    <div className="Recomended__image">
+
+      {typeSite === 'youtube' ? (
+        <img className="img_Online" src={YoutubeIcon} />
+      ) : (
+        <img className="img_Online" src={WebIcon} />
+      )
+      }
+    </div>
+    <div className="Recomended__content">
+      <p>{tittle}</p>
+      <h2 className="tittle_recomended">{subtitle}</h2>
+    </div>
+  </div>
+);
 
 export default Plataform;
