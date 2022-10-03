bdae564d7cfc631e407d9ae3c7f8cea519def726
Alfonso Chavarro <alfonsocortezz25@gmail.com>
2022-10-03 16:41:43 -0500

    Added functionality in the register component

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
+import { register } from "../../services/service";
 import logoIcon from "../../assets/icons/logo.svg";
 import "./Register.css";
 
 const Register = () => {
+  const [fullname, setFullname] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [confirmpassword, setConfirmPassword] = useState("")
+  const [error, setError] = useState({error: false, message: ""})
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if(password !== confirmpassword) {
+      return setError({error: true, message: "Las contraseñas no son iguales"})
+    } else {
+      setError({error: false, message: ""})
+    }
+
+    register({ fullname, email, password })
+      .then((res) => {
+        if (res.error) return setError({error: res.error, message: res.message})
+        setFullname('')
+        setEmail('')
+        setPassword('')
+        setConfirmPassword('')
+        navigate('/formulario/login')
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+  }
+  
   return (
     <div className="Register">
       <div className="Register__form">
@@ -15,18 +47,26 @@ const Register = () => {
         <h2>Crear una nueva cuenta</h2>
         <p>Bienvenido a la familia. Ingresa los datos para crear la cuenta.</p>
 
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <div className="InputsGroup">
             <label htmlFor="fullname">Nombre</label>
             <input
               type="text"
               name="fullname"
               placeholder="Escribe tu nombre"
+              required
+              onChange={(e) => setFullname(e.target.value)}
             />
           </div>
           <div className="InputsGroup">
             <label htmlFor="email">E-mail</label>
-            <input type="email" name="email" placeholder="Escribe tu correo" />
+            <input 
+              type="email" 
+              name="email" 
+              placeholder="Escribe tu correo" 
+              required
+              onChange={(e) => setEmail(e.target.value)}
+              />
           </div>
           <div className="InputsGroup">
             <label htmlFor="password">Contraseña</label>
@@ -34,7 +74,9 @@ const Register = () => {
               type="password"
               name="password"
               placeholder="Escribe tu contraseña"
-            />
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              />
           </div>
           <div className="InputsGroup">
             <label htmlFor="confirmpassword">Confirmar contraseña</label>
@@ -42,9 +84,12 @@ const Register = () => {
               type="password"
               name="confirmpassword"
               placeholder="Escribe de nuevo tu contraseña"
-            />
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+              />
           </div>
-          <button type="button" className="btnStandard btnDark">
+          {error && (<p className="ErrorMessage"> {error.message}</p>)}
+          <button type="submit" className="btnStandard btnDark">
             Crear cuenta
           </button>
           <p className="MessageForm">
