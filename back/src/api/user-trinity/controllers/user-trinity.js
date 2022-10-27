ffaa103c0feaba0ac504a73c6d49f6475f47d492
Juan Esteban <juanestban000@gmail.com>
2022-10-27 01:08:34 -0500

    feat: creating table users for trinity

---
+++ back/src/api/user-trinity/controllers/user-trinity.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * user-trinity controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::user-trinity.user-trinity');
