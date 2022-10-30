1e87c8302238365ca3312e10a1f71a8c84d701e7
Jhonnatan <Jhonnmorenot97@gmail.com>
2022-10-29 21:32:41 -0500

    feat: roles trinity and user anthentication created

---
+++ back/src/api/rol-trinity/services/rol-trinity.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * rol-trinity service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::rol-trinity.rol-trinity');
