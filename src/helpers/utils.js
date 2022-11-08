19e5b9002ca127b9029ba7179780a7da7743864d
Alfonso Chavarro <alfonsocortezz25@gmail.com>
2022-11-08 16:31:12 -0500

    Added validURL function

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -5,4 +5,13 @@ const numberGenerateRandomly = (min, max) => {
 
 const generateRandomUsername = (fullname) => `${fullname.replaceAll(' ', '_')}_${numberGenerateRandomly(0, 15000)}_${Date.now()}`
 
-export { numberGenerateRandomly, generateRandomUsername }
+function validURL(str) {
+  const pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+    '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+  return !!pattern.test(str);
+}
+
+export { numberGenerateRandomly, generateRandomUsername, validURL }
