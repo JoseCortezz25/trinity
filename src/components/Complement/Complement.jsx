3a05b681bb997efb31895c1521fc54143fdafe58
Alfonso Chavarro <alfonsocortezz25@gmail.com>
2022-11-14 19:46:54 -0500

    chore: change of order in the mission and vision

--- src/components/Complement/Complement.jsx
+++ src/components/Complement/Complement.jsx
@@ -4,16 +4,6 @@ const Complement = () => {
   return (
     <section className="Complement">
       <div className="boxInfo">
-        <h3>{'<Visión />'}</h3>
-        <p>
-          Ser pioneros para el 2028 en desarrollo de tecnologías de última
-          generación a nivel departamental y nacional, destacándonos por nuestro
-          cumplimiento, disciplina, creatividad e innovación, proporcionando
-          soluciones tecnológicas de alta calidad, impulsando el desarrollo
-          social de nuestro país.
-        </p>
-      </div>
-      <div className="boxInfo">
         <h3>{'<Misión />'}</h3>
         <p>
           Proveer soluciones de alta calidad e innovación en el mercado
@@ -21,6 +11,16 @@ const Complement = () => {
           procesos, mediante el desarrollo de software acorde a las necesidades
           de nuestros clientes; impulsando el crecimiento y habilidades de
           nuestro grupo de colaboradores.
+        </p>
+      </div>
+      <div className="boxInfo">
+        <h3>{'<Visión />'}</h3>
+        <p>
+          Ser pioneros para el 2028 en desarrollo de tecnologías de última
+          generación a nivel departamental y nacional, destacándonos por nuestro
+          cumplimiento, disciplina, creatividad e innovación, proporcionando
+          soluciones tecnológicas de alta calidad, impulsando el desarrollo
+          social de nuestro país.
         </p>
       </div>
     </section>
