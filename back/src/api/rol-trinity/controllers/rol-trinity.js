12544b6514da3e3fbe10f70fedd0fd83445d20d6
Juan Esteban <juanestban000@gmail.com>
2022-11-18 18:26:12 -0500

    refactor: cleanned file

--- back/src/api/rol-trinity/controllers/rol-trinity.js
+++ back/src/api/rol-trinity/controllers/rol-trinity.js
@@ -1,55 +1,25 @@
-'use strict';
-
-const { findDOMNode } = require('react-dom');
+'use strict'
 
 /**
  * rol-trinity controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require('@strapi/strapi').factories
 
-module.exports = createCoreController('api::rol-trinity.rol-trinity', ({ strapi }) => ({
-    //Method 1: Creating an entirely custom action
-    async exampleAction(ctx) {
-        try {
-            ctx.body = 'ok';
-        } catch (err) {
-          ctx.body = err;
-        }
-    },
-    
- 
-
-    //Method 2: Wrapping a core action (leaves core logic in place)
-
+module.exports = createCoreController(
+  'api::rol-trinity.rol-trinity',
+  ({ strapi }) => ({
     async find(ctx) {
+      ctx.query = { ...ctx.query, local: 'en' }
+      const { data } = await super.find(ctx)
 
-        // some custom logic here
-        ctx.query = { ...ctx.query, local: 'en' };
-
-        // Calling the default core action 
-
-        const { data } = await super.find(ctx);
-
-        // Some more custom logic
-
-
-        return data;
+      return data
     },
-
     async findOne(ctx) {
+      ctx.query = { ...ctx.query, local: 'en' }
+      const { data } = await super.findOne(ctx)
 
-        // some custom logic here
-        ctx.query = { ...ctx.query, local: 'en' };
-
-        // Calling the default core action 
-
-        const { data } = await super.findOne(ctx);
-
-        // Some more custom logic
-
-
-        return data;
-    }
- 
-}))
+      return data
+    },
+  })
+)
