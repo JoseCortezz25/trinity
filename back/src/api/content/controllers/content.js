701b647bae6c7869479df6d5482a3efa1cb9a58f
Juan Esteban <juanestban000@gmail.com>
2022-11-18 20:48:19 -0500

    feat: finised data preprosessing

--- back/src/api/content/controllers/content.js
+++ back/src/api/content/controllers/content.js
@@ -1,9 +1,23 @@
-'use strict';
+'use strict'
 
 /**
  * content controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require('@strapi/strapi').factories
 
-module.exports = createCoreController('api::content.content');
+module.exports = createCoreController('api::content.content', ({ strapi }) => ({
+  async find(ctx) {
+    ctx.query = { ...ctx.query, local: 'en' }
+    const { data: result, meta } = await super.find(ctx)
+    const { pagination } = meta
+
+    return { result, pagination }
+  },
+  async findOne(ctx) {
+    ctx.query = { ...ctx.query, local: 'en' }
+    const { data: result } = await super.findOne(ctx)
+
+    return result
+  },
+}))
--- back/src/api/path-learning/controllers/path-learning.js
+++ back/src/api/path-learning/controllers/path-learning.js
@@ -1,9 +1,26 @@
-'use strict';
+'use strict'
 
 /**
  * path-learning controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require('@strapi/strapi').factories
 
-module.exports = createCoreController('api::path-learning.path-learning');
+module.exports = createCoreController(
+  'api::path-learning.path-learning',
+  ({ strapi }) => ({
+    async find(ctx) {
+      ctx.query = { ...ctx.query, local: 'en' }
+      const { data: result, meta } = await super.find(ctx)
+      const { pagination } = meta
+
+      return { result, pagination }
+    },
+    async findOne(ctx) {
+      ctx.query = { ...ctx.query, local: 'en' }
+      const { data: result } = await super.findOne(ctx)
+
+      return result
+    },
+  })
+)
